2686e8edee551f2141f8ccadcb837c7a81fa0a91
iam-siva25 <sivabalasubramanian2502@gmail.com>
2026-03-11 21:30:45 +0530

    fix: force static sitemap for export

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,4 +1,7 @@
 import { MetadataRoute } from 'next'
+
+// Add this line to satisfy the "output: export" requirement
+export const dynamic = "force-static";
  
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
